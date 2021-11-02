70ff1c6b3efcde212394f95b821f2e96f5d77a04
yerloveraaa <yerlover@gmail.com>
2021-11-02 09:52:44 -0400

    Update firebaseConfig.js
    update firebese Config

--- src/components/firebase/firebaseConfig.js
+++ src/components/firebase/firebaseConfig.js
@@ -5,14 +5,13 @@ import 'firebase/storage';
 import 'firebase/messaging'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBsv2UvDfWjm6f31B7T1GPEMA0q_MULVZk",
-  authDomain: "jms-eats-70330.firebaseapp.com",
-  databaseURL: "https://jms-eats-70330-default-rtdb.firebaseio.com",
-  projectId: "jms-eats-70330",
-  storageBucket: "jms-eats-70330.appspot.com",
-  messagingSenderId: "371776288397",
-  appId: "1:371776288397:web:41234305fbea4a40ac54c4",
-  measurementId: "G-8688R4M7VM"
+  apiKey: "AIzaSyDW_MZwQ4wqfJpgLHdbrmXzAODZ1NqtLL8",
+  authDomain: "restaurant-2fe17.firebaseapp.com",
+  databaseURL: "https://restaurant-2fe17-default-rtdb.firebaseio.com",
+  projectId: "restaurant-2fe17",
+  storageBucket: "restaurant-2fe17.appspot.com",
+  messagingSenderId: "1031122206368",
+  appId: "1:1031122206368:web:777970f07ece570da853c5"
 };
 
 firebase.initializeApp(firebaseConfig);
